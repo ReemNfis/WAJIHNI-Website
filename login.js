3f341bcf0bef71039d2e6564c0b628bce3aac6f5
R <104394097+Rr2410@users.noreply.github.com>
2023-03-28 21:04:35 +0300

    login page edits

--- login.js
+++ login.js
@@ -1,25 +1,11 @@
-const form = document.getElementById("submitForm");
-const UserName = document.getElementById("usernamee").value;
-const Password = document.getElementById("passwordd").value;
-const email = "editor@imamu.com";
-const pass = "helloworld1";
-function validateForm () {
-      if (UserName == "" || UserName == null) {
-          alert("يرجى إدخال البريد الإلكتروني");
-          return;
-      }
-      if (Password == "" || Password == null) {
-          alert("يرجى إدخال كلمة المرور");
-          return;
-
-      }
-      if (UserName != email || Password != pass) {
-          alert("اسم المستخدم و/أو كلمة المرور غير صحيحة");
-          return;
-      }
-      if (UserName == email && Password == pass) {
-        alert("form submitted successfully")
-		return true;
-      }
+function auth() {
+  var email = document.getElementById("usernamee").value;
+  var password = document.getElementById("passwordd").value;
+  if (email.value == "admin@imamu.com" && password.value == "admin123") {
+    window.location.assign("editorhomepage.html");
+    alert("Login Successfully ");
+  } else {
+    alert("Invalid Information ");
+    return;
+  }
 }
-
