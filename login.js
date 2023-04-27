0fa4ed4156f23dcf6bd4c267fb886b440b255b46
Rr2410 <123rr456aa@gmail.com>
2023-04-27 11:46:50 +0300

    removed the search page and replaced with a search bar in the main homepage

--- login.js
+++ login.js
@@ -5,9 +5,11 @@ function validation() {
     "username",
     document.getElementById("usernamee").value
   );
+   const admin = "admin@imamu.com"
+   const pass ="admin123"
   var email = document.getElementById("usernamee").value;
   var password = document.getElementById("passwordd").value;
-  if (email == "admin@imamu.com" && password == "admin123") {
+  if (email == admin && password == pass ) {
     window.location.assign("editorhomepage.html");
     alert("تم تسجيل الدخول بنجاح ");
   } else {
