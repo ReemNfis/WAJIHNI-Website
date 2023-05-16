ff390bcca349fe39ecfa14a4fa6c306ba626e452
Rr2410 <123rr456aa@gmail.com>
2023-05-16 12:40:27 +0300

    frontend for 'adding office' page with styling

--- homepage.js
+++ homepage.js
@@ -3,35 +3,4 @@ function Search() {
   window.location.assign("login.html");
 }
 
-  function floorDisplayCheck(floorNumber) {
-    if (floorNumber<=4 && floorNumber>=0) {
-      if(floorNumber==0) {
-        displayFloor0()
-      }if(floorNumber==1){
-        displayFloor1()
-      }if(floorNumber==2){
-        displayFloor2()
-      }if(floorNumber==3){
-        displayFloor3()
-      }if(floorNumber==4){
-        displayFloor4()
-      }
-    } else {
-      console.warn("هذا الطابق غير موجود، يمكنك البحث عن الطابق بالارقام 1 او 2 او 3 او4 او 0 فقط");
-    }
-  }
-  function displayFloor0() {
-    
-  }
-  function displayFloor1() {
-    
-  }
-  function displayFloor2() {
-    
-  }
-  function displayFloor3() {
-    
-  }
-  function displayFloor4() {
-    
-  }
+  
